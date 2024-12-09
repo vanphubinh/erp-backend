80a48028afb1365a073c7d465dc9d4788b28c25b
An <an.tran511@outlook.com>
2024-12-09 17:55:54 +0700

    feat: config db connections to write and read replica

--- infra/src/state.rs
+++ infra/src/state.rs
@@ -1,22 +1,6 @@
-use rand::seq::SliceRandom;
 use sea_orm::DatabaseConnection;
 use std::sync::Arc;
 pub struct AppState {
-  write_db: Arc<DatabaseConnection>,
-  read_dbs: Vec<Arc<DatabaseConnection>>,
-}
-
-impl AppState {
-  pub fn new(write_db: Arc<DatabaseConnection>, read_dbs: Vec<Arc<DatabaseConnection>>) -> Self {
-    Self { write_db, read_dbs }
-  }
-
-  pub fn get_read_db(&self) -> Arc<DatabaseConnection> {
-    let mut rng = rand::thread_rng();
-    self.read_dbs.choose(&mut rng).unwrap().clone()
-  }
-
-  pub fn get_write_db(&self) -> Arc<DatabaseConnection> {
-    self.write_db.clone()
-  }
+  pub write_db: Arc<DatabaseConnection>,
+  pub read_db: Arc<DatabaseConnection>,
 }
--- server/src/lib.rs
+++ server/src/lib.rs
@@ -1,6 +1,7 @@
 use axum::Router;
+use infra::state::AppState;
 use sea_orm::{Database, DatabaseConnection, DbErr};
-use std::net::SocketAddr;
+use std::{net::SocketAddr, sync::Arc};
 use tokio::net::TcpListener;
 use tower_http::trace::{self, TraceLayer};
 use tracing::Level;
@@ -14,19 +15,47 @@ pub async fn start() {
     .with_test_writer()
     .init();
 
+  tracing::info!("Connecting to databases...");
+  let write_db = match get_db_connection("DATABASE_URL").await {
+    Ok(db) => {
+      tracing::info!("Connected to write database!");
+      db
+    }
+    Err(_) => {
+      tracing::error!("Failed to connect to write database!");
+      return;
+    }
+  };
+  let read_db = match get_db_connection("DATABASE_URL_READ").await {
+    Ok(db) => {
+      tracing::info!("Connected to read database!");
+      db
+    }
+    Err(_) => {
+      tracing::error!("Failed to connect to read database!");
+      return;
+    }
+  };
+  let app_state = Arc::new(AppState {
+    write_db: Arc::new(write_db),
+    read_db: Arc::new(read_db),
+  });
+
   let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
   let tcp = TcpListener::bind(&addr).await.unwrap();
 
-  let router = Router::new().layer(
-    TraceLayer::new_for_http().make_span_with(trace::DefaultMakeSpan::new().level(Level::INFO)),
-  );
+  let router = Router::new()
+    .layer(
+      TraceLayer::new_for_http().make_span_with(trace::DefaultMakeSpan::new().level(Level::INFO)),
+    )
+    .with_state(app_state.clone());
 
   tracing::debug!("Listening on http://{}", addr);
   axum::serve(tcp, router).await.unwrap();
 }
 
-pub async fn get_db_connection() -> Result<DatabaseConnection, DbErr> {
-  let db_url = std::env::var("DATABASE_URL").unwrap();
+pub async fn get_db_connection(env_var: &str) -> Result<DatabaseConnection, DbErr> {
+  let db_url = std::env::var(env_var).unwrap();
   let db = Database::connect(&db_url).await?;
   Ok(db)
 }
