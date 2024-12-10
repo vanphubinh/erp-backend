0acf2e585d0ac4e3637ff65db43e60574742f095
An <an.tran511@outlook.com>
2024-12-10 09:35:05 +0700

    feat: category model and migration

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -1,12 +1,16 @@
 pub use sea_orm_migration::prelude::*;
 
 mod m20241209_113221_create_uom_table;
+mod m20241210_023245_create_category_table;
 
 pub struct Migrator;
 
 #[async_trait::async_trait]
 impl MigratorTrait for Migrator {
   fn migrations() -> Vec<Box<dyn MigrationTrait>> {
-    vec![Box::new(m20241209_113221_create_uom_table::Migration)]
+    vec![
+            Box::new(m20241209_113221_create_uom_table::Migration),
+            Box::new(m20241210_023245_create_category_table::Migration),
+        ]
   }
 }
