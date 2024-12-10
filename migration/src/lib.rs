2a56962857de5409adb5e7dc0c0e33d795b4720c
An <an.tran511@outlook.com>
2024-12-10 10:14:38 +0700

    feat: add attribute and attribute option domain

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -2,6 +2,8 @@ pub use sea_orm_migration::prelude::*;
 
 mod m20241209_113221_create_uom_table;
 mod m20241210_023245_create_category_table;
+mod m20241210_030648_create_attribute_table;
+mod m20241210_031041_create_attribute_option_table;
 
 pub struct Migrator;
 
@@ -11,6 +13,8 @@ impl MigratorTrait for Migrator {
     vec![
             Box::new(m20241209_113221_create_uom_table::Migration),
             Box::new(m20241210_023245_create_category_table::Migration),
+            Box::new(m20241210_030648_create_attribute_table::Migration),
+            Box::new(m20241210_031041_create_attribute_option_table::Migration),
         ]
   }
 }
